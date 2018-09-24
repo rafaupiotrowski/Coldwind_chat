d0d39157312c141bdef6ed42297c6987d600ff41
rafaupiotrowski <43062913+rafaupiotrowski@users.noreply.github.com>
2018-09-24 22:07:43 +0200

    Wersja 0.6
    Budowanie nagłówka odpowiedzi na podstawie kodu zwróconego przez metodę obsługującą żądanie.

--- HttpServer.java
+++ HttpServer.java
@@ -32,9 +32,13 @@ class HttpServerHandler implements Runnable {
 	
 	Socket incoming;
 	String line;
-	PrintWriter answer;
+	String handleAnswer;
+	String statusCode;
+	HashMap <String, String> statusCodeDescription = new HashMap<>();
+
 	public HttpServerHandler (Socket incomingConnection){
 		incoming = incomingConnection;
+		statusCodeDescription.put("200", "OK!");
 	}
 	
 	public void run(){
@@ -49,7 +53,6 @@ class HttpServerHandler implements Runnable {
 				if(line.contentEquals("")) break;
 				lines.add(line);
 			};
-//			System.out.println(lines);
 			headerTokens =lines.get(0).split(" ");
 			System.out.println("Nagłówek: " +Arrays.toString(headerTokens));
 			if (headerTokens.length != 3){
@@ -59,15 +62,14 @@ class HttpServerHandler implements Runnable {
 			if(headerTokens[0].equals("GET")) {
 				System.out.println("Obsługa żądania typu GET");
 				if (headerTokens[1].equals("/") || headerTokens[1].equals("/index.html")){
-					String indexHtml =handleGetIndex();
-					out.print("HTTP/1.1 200 OK \r\n");
-					out.print("Content-Type: text/html; charset=utf-8\r\n");
-					out.print("\r\n");
-					out.print(indexHtml);
-					out.flush();
-					out.close();
-					incoming.close();
+					handleAnswer = handleGetIndex();
 				}
+				out.print("HTTP/1.1 " + statusCode + " " + statusCodeDescription.get(statusCode) + "\r\n");
+				out.print("Content-Type: text/html; charset=utf-8\r\n");
+				out.print("\r\n");
+				out.print(handleAnswer);
+				out.close();
+				incoming.close();
 				if (headerTokens[1].equals("/style.css")) handleGetStyle();
 			}
 			
@@ -82,7 +84,7 @@ class HttpServerHandler implements Runnable {
 		System.out.println("Wywołano metodę handleGetIndex");
 		Path pathToIndex =Paths.get("httpChatIndex.html");
 		String htmlContent =new String (Files.readAllBytes(pathToIndex));
-	//	System.out.println(htmlContent);
+		statusCode = "404";
 		return htmlContent;
 	}	
 	private void handleGetStyle(){
