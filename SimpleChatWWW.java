177d3e1fd149d9a22c9a58750c00fa064f4bad07
rafaupiotrowski <43062913+rafaupiotrowski@users.noreply.github.com>
2018-09-30 13:09:01 +0200

    Update SimpleChatWWW.java
    Metody do obsługi żądań POST i GET.

--- SimpleChatWWW.java
+++ SimpleChatWWW.java
@@ -11,9 +11,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class SimpleChatWWW {
-	
-	
-	Socket incoming;
+		
 	String line;
 	String handleAnswer;
 	String statusCode;
@@ -25,11 +23,10 @@ public class SimpleChatWWW {
 	HashMap<String, String> resources = new HashMap<>();
 	HashMap<String, String> contentType = new HashMap<>();
 
-	public HttpServerHandler (Socket incomingConnection){
+	public SimpleChatWWW (){
 		data ="";
 		last_message_id=0;
 		gson =new GsonBuilder().create();
-		incoming = incomingConnection;
 		statusCodeDescription.put("200", "OK!");
 		resources.put("/", "httpChatIndex.html");
 		resources.put("/index.html", "httpChatIndex.html");	
@@ -41,27 +38,29 @@ public class SimpleChatWWW {
 		contentType.put("/main.js", "application/javascript");
 	}
 	
-	public String handleHttpRequest(HashMap<String, String> aRequest){
-		return "nic";
+	public String handleHttpRequest(HashMap<String, String> aRequest) throws IOException{
+		System.out.println("Wywołano metodę: handleHTTPRequest");
+		if(aRequest.get("method").equals("GET")){
+			return handleGet(aRequest);
+		}
+		else if(aRequest.get("method").equals("POST")){
+			return handlePost(aRequest);
+		}
+		else return "Błąd";
 	}
-	
-	if(headerTokens[0].equals("GET")) {
-		System.out.println("Obsługa żądania typu GET");
-		handleAnswer =handleGet(headerTokens[1]);
-	}
-	else if (headerTokens[0].equals("POST")){
+
+	private String handlePost(HashMap<String, String> aRequest) throws IOException{
 		System.out.println("Obsługa żądania POST");
-		System.out.println(lines);
-		data =new String(formData);
-		System.out.println(data);
-		handleAnswer ="test";
+		return "test";
 	}
-	private String handleGet(String aResource) throws IOException {
+	private String handleGet(HashMap<String, String> aRequest) throws IOException {
+		String aResource =aRequest.get("query");
 		if(resources.containsKey(aResource)){
 			resource = resources.get(aResource);
 			Path pathToIndex =Paths.get(resource);
 			String htmlContent =new String (Files.readAllBytes(pathToIndex));
 			statusCode = "200";
+//			System.out.println(htmlContent);
 			return htmlContent;
 		} else{
 			System.out.println("Nieprawidłowe żądanie zasobu: " + aResource);
@@ -69,9 +68,4 @@ public class SimpleChatWWW {
 			return "";
 		}		
 	}
-
-	public SimpleChatWWW() {
-		// TODO Auto-generated constructor stub
-	}
-
 }
