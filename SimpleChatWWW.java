bd99c0cee1234bbf4fd351da87be5c80efeff17a
rafaupiotrowski <43062913+rafaupiotrowski@users.noreply.github.com>
2018-10-07 19:48:45 +0200

    Wersja 1.2
    Prawie działa...

--- SimpleChatWWW.java
+++ SimpleChatWWW.java
@@ -5,12 +5,17 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 public class SimpleChatWWW {
 		
@@ -24,15 +29,22 @@ public class SimpleChatWWW {
 	Gson gson;
 	JsonParser parser = new JsonParser();
 	JsonObject last_message_id_json = new JsonObject();
+	JsonObject text = new JsonObject();
+	String[] message= new String[2];
+	String[] testMessages = new String[1]; 
 	HashMap <String, String> statusCodeDescription = new HashMap<>();
 	HashMap<String, String> resources = new HashMap<>();
 	HashMap<String, String> contentType = new HashMap<>();
-	
+	ArrayList<String[]> allMessages;
+	String messagesToSendJson;
 
 	public SimpleChatWWW (){
+		gson =new GsonBuilder().create();
 		data ="";
 		last_message_id=0;
-		gson =new GsonBuilder().create();
+		testMessages[0] ="{\"last_message_id\": \"1\", \"testMessages\":[ [\"100\", \"heja\"], [\"200\", \"joł\"]] }";
+		String messagesToSend;
+		allMessages = new ArrayList<String[]>();
 		statusCodeDescription.put("200", "OK!");
 		resources.put("/", "httpChatIndex.html");
 		resources.put("/index.html", "httpChatIndex.html");	
@@ -62,12 +74,43 @@ public class SimpleChatWWW {
 
 	private HashMap<String, String> handlePost(HashMap<String, String> aRequest) throws IOException{
 		System.out.println("Obsługa żądania POST");
+		HashMap<String, String> response = new HashMap<>();
 		if(aRequest.get("query").equals("/messages")) {
 			last_message_id_json = parser.parse(aRequest.get("data")).getAsJsonObject();
 			last_message_id =last_message_id_json.get("last_message_id").getAsInt();
-			System.out.println("Odczytano last_message_id: " + last_message_id);
+			if (last_message_id <0) last_message_id =0;
+			System.out.println("Odczytano last_messages_id: " + last_message_id);
+			ArrayList<String[]> messagesToSend =new ArrayList<>(allMessages.subList(last_message_id, allMessages.size()));
+			messagesToSendJson = gson.toJson(messagesToSend);
+			JsonObject test = new JsonObject();
+			JsonPrimitive currentMessageId = new JsonPrimitive(allMessages.size()); //allMessages.size();
+			JsonArray messagesToSendTest =new JsonArray();
+			for(int i=0; i<allMessages.size(); i++){
+				String element1 =allMessages.get(i)[0];
+				String element2 =allMessages.get(i)[1];
+				JsonArray singleArray = new JsonArray();
+				singleArray.add(element1);
+				singleArray.add(element2);
+				messagesToSendTest.add(singleArray);
+			}
+			System.out.println(messagesToSendTest.toString());
+			test.add("last_message_id", currentMessageId);
+			test.add("messages", messagesToSendTest);
+				response.put("statusCode", "200");
+				response.put("statusCodeDescription", "OK");
+				response.put("data", test.toString());
+				response.put("contentType", "application/json");
+				System.out.println("Wysłano" + response);
+
+		} else if(aRequest.get("query").equals("/chat")){
+			message[0] = "123";
+			text =parser.parse(aRequest.get("data")).getAsJsonObject();
+			message[1] = text.get("text").getAsString();
+			allMessages.add(message);
+			response.put("statusCode", "200");
+			response.put("statusCodeDescription", "OK");
+			System.out.println(Arrays.toString(message));
 		}
-		HashMap<String, String> response = new HashMap<>();
 		return response;
 	}
 	
