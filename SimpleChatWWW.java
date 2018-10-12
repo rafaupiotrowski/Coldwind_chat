837ba4e9438d8509653ea3b68d4d99a669019465
rafaupiotrowski <43062913+rafaupiotrowski@users.noreply.github.com>
2018-10-12 21:15:00 +0200

    Update SimpleChatWWW.java

--- SimpleChatWWW.java
+++ SimpleChatWWW.java
@@ -1,7 +1,6 @@
 package http_chat;
 
 import java.io.IOException;
-import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,29 +11,25 @@ import java.util.HashMap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 
 public class SimpleChatWWW {
 		
-	String line;
-	String handleAnswer;
 	String resource;
 	String data;
 	int newest_message_id;
 	int last_message_id;
+	
 	Gson gson;
 	JsonParser parser = new JsonParser();
 	JsonObject last_message_id_json = new JsonObject();
 	JsonObject text = new JsonObject();
 	
-	HashMap <String, String> statusCodeDescription = new HashMap<>();
 	HashMap<String, String> resources = new HashMap<>();
 	HashMap<String, String> contentType = new HashMap<>();
 	ArrayList<String[]> allMessages;
-	String messagesToSendJson;
 
 	public SimpleChatWWW (){
 		gson =new GsonBuilder().create();
@@ -52,7 +47,7 @@ public class SimpleChatWWW {
 	}
 	
 	public HashMap<String, String> handleHttpRequest(HashMap<String, String> aRequest) throws IOException{
-		System.out.println("Wywołano metodę: handleHTTPRequest");
+//		System.out.println("Wywołano metodę: handleHTTPRequest");
 		HashMap<String, String> response = new HashMap<>();
 		if(aRequest.get("method").equals("GET")){
 			return handleGet(aRequest);
@@ -74,28 +69,26 @@ public class SimpleChatWWW {
 			last_message_id_json = parser.parse(aRequest.get("data")).getAsJsonObject();
 			last_message_id =last_message_id_json.get("last_message_id").getAsInt();
 			if (last_message_id <0) last_message_id =0;
-			System.out.println("Odczytano last_messages_id: " + last_message_id);
-			ArrayList<String[]> messagesToSend =new ArrayList<>(allMessages.subList(last_message_id, allMessages.size()));
-			messagesToSendJson = gson.toJson(messagesToSend);
-			JsonObject test = new JsonObject();
-			JsonPrimitive currentMessageId = new JsonPrimitive(allMessages.size()); //allMessages.size();
-			JsonArray messagesToSendTest =new JsonArray();
+//			System.out.println("Odczytano last_messages_id: " + last_message_id);
+			JsonObject messages = new JsonObject();
+			JsonPrimitive currentMessageId = new JsonPrimitive(allMessages.size());
+			JsonArray messagesToSend =new JsonArray();
 			for(int i=last_message_id; i<allMessages.size(); i++){
 				String element1 =allMessages.get(i)[0];
 				String element2 =allMessages.get(i)[1];
 				JsonArray singleArray = new JsonArray();
 				singleArray.add(element1);
 				singleArray.add(element2);
-				messagesToSendTest.add(singleArray);
+				messagesToSend.add(singleArray);
 			}
-			System.out.println(messagesToSendTest.toString());
-			test.add("last_message_id", currentMessageId);
-			test.add("messages", messagesToSendTest);
+//			System.out.println(messagesToSend.toString());
+			messages.add("last_message_id", currentMessageId);
+			messages.add("messages", messagesToSend);
 			response.put("statusCode", "200");
 			response.put("statusCodeDescription", "OK");
-			response.put("data", test.toString());
+			response.put("data", messages.toString());
 			response.put("contentType", "application/json");
-			System.out.println("Wysłano" + response);
+//			System.out.println("Wysłano" + response);
 
 		} else if(aRequest.get("query").equals("/chat")){
 			String[] message= new String[2];
