67df5a4e15c92c66f851177c831f10979c752442
rafaupiotrowski <43062913+rafaupiotrowski@users.noreply.github.com>
2018-10-14 20:02:37 +0200

    Update SimpleChatWWW.java

--- SimpleChatWWW.java
+++ SimpleChatWWW.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -19,12 +20,12 @@ public class SimpleChatWWW {
 		
 	String resource;
 	String data;
-	int newest_message_id;
+	static int newest_message_id;
 	int last_message_id;
 	
 	Gson gson;
 	JsonParser parser = new JsonParser();
-	JsonObject last_message_id_json = new JsonObject();
+	JsonObject receivedText = new JsonObject();
 	JsonObject text = new JsonObject();
 	
 	HashMap<String, String> resources = new HashMap<>();
@@ -46,8 +47,7 @@ public class SimpleChatWWW {
 		contentType.put("/main.js", "application/javascript");
 	}
 	
-	public HashMap<String, String> handleHttpRequest(HashMap<String, String> aRequest) throws IOException{
-//		System.out.println("Wywołano metodę: handleHTTPRequest");
+	public HashMap<String, String> handleHttpRequest(Map<String, String> aRequest) throws IOException{
 		HashMap<String, String> response = new HashMap<>();
 		if(aRequest.get("method").equals("GET")){
 			return handleGet(aRequest);
@@ -62,33 +62,22 @@ public class SimpleChatWWW {
 		}
 	}
 
-	private HashMap<String, String> handlePost(HashMap<String, String> aRequest) throws IOException{
+	private HashMap<String, String> handlePost(Map<String, String> aRequest) throws IOException{
 		System.out.println("Obsługa żądania POST");
 		HashMap<String, String> response = new HashMap<>();
 		if(aRequest.get("query").equals("/messages")) {
-			last_message_id_json = parser.parse(aRequest.get("data")).getAsJsonObject();
-			last_message_id =last_message_id_json.get("last_message_id").getAsInt();
+			receivedText = parser.parse(aRequest.get("data")).getAsJsonObject();
+			last_message_id =receivedText.get("last_message_id").getAsInt();
 			if (last_message_id <0) last_message_id =0;
-//			System.out.println("Odczytano last_messages_id: " + last_message_id);
-			JsonObject messages = new JsonObject();
-			JsonPrimitive currentMessageId = new JsonPrimitive(allMessages.size());
-			JsonArray messagesToSend =new JsonArray();
-			for(int i=last_message_id; i<allMessages.size(); i++){
-				String element1 =allMessages.get(i)[0];
-				String element2 =allMessages.get(i)[1];
-				JsonArray singleArray = new JsonArray();
-				singleArray.add(element1);
-				singleArray.add(element2);
-				messagesToSend.add(singleArray);
-			}
-//			System.out.println(messagesToSend.toString());
-			messages.add("last_message_id", currentMessageId);
-			messages.add("messages", messagesToSend);
+			JsonObject text = new JsonObject();
+			JsonPrimitive currentMessageId = new JsonPrimitive(newest_message_id);
+			JsonArray messagesToSend = jsonfyMessages(last_message_id);
+			text.add("last_message_id", currentMessageId);
+			text.add("messages", messagesToSend);
 			response.put("statusCode", "200");
 			response.put("statusCodeDescription", "OK");
-			response.put("data", messages.toString());
+			response.put("data", text.toString());
 			response.put("contentType", "application/json");
-//			System.out.println("Wysłano" + response);
 
 		} else if(aRequest.get("query").equals("/chat")){
 			String[] message= new String[2];
@@ -98,15 +87,25 @@ public class SimpleChatWWW {
 			allMessages.add(message);
 			response.put("statusCode", "200");
 			response.put("statusCodeDescription", "OK");
-			System.out.println(Arrays.toString(message));
-			for(String[] test: allMessages){
-			System.out.println("Wszystkie wiadomości: " +Arrays.toString(test));
-			}
+			newest_message_id++;
 			}
 		return response;
 	}
 	
-	private HashMap<String, String> handleGet(HashMap<String, String> aRequest) throws IOException {		
+	private JsonArray jsonfyMessages(int last_message_id){
+		JsonArray answer = new JsonArray();	
+		for(int i=last_message_id; i<newest_message_id; i++){
+			String element1 =allMessages.get(i)[0];
+			String element2 =allMessages.get(i)[1];
+			JsonArray singleArray = new JsonArray();
+			singleArray.add(element1);
+			singleArray.add(element2);
+			answer.add(singleArray);
+		}
+		return answer;
+	}
+	
+	private HashMap<String, String> handleGet(Map<String, String> aRequest) throws IOException {		
 		HashMap<String, String> response = new HashMap<>();
 		String aResource =aRequest.get("query");
 		if(resources.containsKey(aResource)){
